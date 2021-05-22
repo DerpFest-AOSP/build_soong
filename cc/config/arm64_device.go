bc2a4fa7a0101514806215cf16fbd1396b13548d
NurKeinNeid <mralexman3000@gmail.com>
2021-05-22 18:23:34 +0200

    Revert "soong: use -mcpu=cortex-a55 for kryo300 and kryo385"
    This reverts commit eddc144cc487b9111108835bbd3a318114d9c615.

--- cc/config/arm64_device.go
+++ cc/config/arm64_device.go
@@ -69,13 +69,9 @@ var (
 		"kryo": []string{
 			"-mcpu=kryo",
 		},
-		"kryo300": []string{
-			// Use cortex-a55 because kryo300 is not supported in GCC/clang.
-			"-mcpu=cortex-a55",
-		},
 		"kryo385": []string{
-			// Use cortex-a55 because kryo385 is not supported in GCC/clang.
-			"-mcpu=cortex-a55",
+			// Use cortex-a53 because kryo385 is not supported in GCC/clang.
+			"-mcpu=cortex-a53",
 		},
 		"exynos-m1": []string{
 			"-mcpu=exynos-m1",
@@ -139,8 +135,7 @@ var (
 		"cortex-a75": "${config.Arm64ClangCortexA55Cflags}",
 		"cortex-a76": "${config.Arm64ClangCortexA55Cflags}",
 		"kryo":       "${config.Arm64ClangKryoCflags}",
-		"kryo300":    "${config.Arm64ClangCortexA55Cflags}",
-		"kryo385":    "${config.Arm64ClangCortexA55Cflags}",
+		"kryo385":    "${config.Arm64ClangCortexA53Cflags}",
 		"exynos-m1":  "${config.Arm64ClangExynosM1Cflags}",
 		"exynos-m2":  "${config.Arm64ClangExynosM2Cflags}",
 	}
