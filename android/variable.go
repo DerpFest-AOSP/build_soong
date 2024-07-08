b8ef0372573abd663e333bc78e6628073b98db2e
Kiyoung Kim <kiyoungkim@google.com>
2024-07-08 11:02:23 +0900

    Add 'use_debug_art' to product_variable
    Add 'use_debug_art' property to product_variable so it can be checked
    from Android.bp whether ART debug APEX or ART APEX should be used.
    
    Bug: 351698657
    Test: AOSP CF eng build succeeded
    Test: AOSP CF userdebug build succeeded
    Change-Id: I01cd80f0d899c55828ecb05fdafdc9bb05fd8200

--- android/variable.go
+++ android/variable.go
@@ -519,6 +519,8 @@ type ProductVariables struct {
 	BoardUseVbmetaDigestInFingerprint *bool `json:",omitempty"`
 
 	OemProperties []string `json:",omitempty"`
+
+	ArtTargetIncludeDebugBuild *bool `json:",omitempty"`
 }
 
 type PartitionQualifiedVariablesType struct {
