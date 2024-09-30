6eac7e6c5bf569d0ff37fb2b01e8f9282d4ad6c0
Ido Ben-Hur <idoybh2@gmail.com>
2024-09-30 02:11:24 +0200

    soong: Return release-keys
    Change-Id: Icbb7fdf23e4a76c542a15693feb46bec99ac8eb7
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- android/config.go
+++ android/config.go
@@ -1027,7 +1027,7 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
-	return "dev-keys"
+	return "release-keys"
 }
 
 func (c *config) ApexKeyDir(ctx ModuleContext) SourcePath {
